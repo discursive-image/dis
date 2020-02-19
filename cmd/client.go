553227a7f69e2c3e04c7475b3e108541651f221c
Daniel Morandini <danielmorandini@me.com>
2020-02-19 15:30:28 +0100

    Add client example

---
+++ cmd/client.go
@@ -0,0 +1,85 @@
+// SPDX-FileCopyrightText: 2020 Jecoz
+//
+// SPDX-License-Identifier: MIT
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/url"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+var arg0 = filepath.Base(os.Args[0])
+
+func logf(format string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, arg0+" * "+format+"\n", args...)
+}
+
+func errorf(format string, args ...interface{}) {
+	logf("error: "+format, args...)
+}
+
+func exitf(format string, args ...interface{}) {
+	errorf(format, args...)
+	os.Exit(1)
+}
+
+func main() {
+	host := flag.String("h", "localhost:7745", "Websocket address to connect to")
+	flag.Parse()
+
+	u := url.URL{Scheme: "ws", Host: *host, Path: "/di/stream"}
+	logf("connecting to %v", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		exitf("dial: %v", err)
+	}
+	defer c.Close()
+
+	// Handle signals.
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	done := make(chan struct{})
+	go func() {
+		defer close(done)
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				errorf("read:", err)
+				return
+			}
+			logf("recv: %s", message)
+		}
+	}()
+
+	for {
+		select {
+		case <-done:
+			return
+		case <-interrupt:
+			logf("interrupt signal received")
+
+			// Cleanly close the connection by sending a close message and then
+			// waiting (with timeout) for the server to close the connection.
+			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				logf("write close:", err)
+				return
+			}
+			select {
+			case <-done:
+			case <-time.After(time.Second):
+			}
+			return
+		}
+	}
+}
--- makefile
+++ makefile
@@ -2,12 +2,14 @@
 #
 # SPDX-License-Identifier: MIT
 
-all: dis examples
+all: dis
 dis: cmd/main.go
 	go build -v -o bin/$@ $^
-examples: ratereader
+examples: ratereader client
 ratereader:
 	go build -v -o bin/$@ cmd/ratereader.go
+client:
+	go build -v -o bin/$@ cmd/client.go
 format:
 	go fmt ./...
 
