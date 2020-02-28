ebcb715a50c497130598235890390cd7d9a4dacb
Daniel Morandini <danielmorandini@me.com>
2020-02-28 11:51:58 +0100

    Wrap things into api/

---
+++ api/fs.go
@@ -0,0 +1,41 @@
+// SPDX-FileCopyrightText: 2020 Jecoz
+//
+// SPDX-License-Identifier: MIT
+
+package api
+
+import (
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+type FileHandler struct {
+	fs  http.Handler
+	dir string
+}
+
+func NewFileHandler(d string) *FileHandler {
+	return &FileHandler{
+		fs:  http.FileServer(http.Dir(d)),
+		dir: d,
+	}
+}
+
+func (f *FileHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	logf("file request from %v: %v", r.RemoteAddr, r.URL.Path)
+	f.fs.ServeHTTP(w, r)
+}
+
+func (f *FileHandler) Exists(path string) bool {
+	_, err := os.Stat(filepath.Join(f.dir, path))
+	return err == nil
+}
+
+func (f *FileHandler) Create(path string) (*os.File, error) {
+	fn := filepath.Join(f.dir, path)
+	if err := os.MkdirAll(filepath.Dir(fn), os.ModePerm); err != nil {
+		return nil, err
+	}
+	return os.Create(fn)
+}
--- cmd/main.go
+++ cmd/main.go
@@ -5,45 +5,30 @@
 package main
 
 import (
-	"bufio"
 	"context"
-	"crypto/md5"
-	"encoding/csv"
-	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
-	"net/url"
 	"os"
 	"os/signal"
 	"path/filepath"
 	"strconv"
-	"strings"
-	"sync"
 	"time"
 
-	"github.com/gorilla/websocket"
+	"github.com/jecoz/dis/api"
 )
 
 var arg0 = filepath.Base(os.Args[0])
 var logger = log.New(os.Stdout, "", log.LstdFlags)
-
-const (
-	// Time allowed to write a message to the peer.
-	writeWait = 10 * time.Second
-
-	// Time to wait before force close on connection.
-	closeGracePeriod = 10 * time.Second
-)
 
 func logf(format string, args ...interface{}) {
 	logger.Printf(arg0+" * "+format, args...)
 }
 
 func errorf(format string, args ...interface{}) {
-	logger.Printf(arg0+" error * "+format, args...)
+	logf(" error: "+format, args...)
 }
 
 func exitf(format string, args ...interface{}) {
@@ -62,314 +47,10 @@ func openInput(path string) (io.ReadCloser, error) {
 	return file, nil
 }
 
-// DI is a DiscoursiveImage.
-type DI struct {
-	Link     string `json:"link"`
-	Word     string `json:"word"`
-	Caption  string `json:"caption"`
-	FileName string `json:"file_name"`
-}
-
-type StreamHandler struct {
-	r       io.Reader
-	sd      string // storage directory path.
-	clients struct {
-		sync.Mutex
-		m map[string]chan *DI
-	}
-	up websocket.Upgrader
-	m  *mapset
-
-	lastDI struct {
-		sync.Mutex
-		val *DI
-	}
-}
-
-type diRx struct {
-	c     chan *DI
-	close func()
-}
-
-// OpenRx returns a new instance of a channel that is registered
-// with the stream handler. Each time a new image is read, it is
-// broadcasted to all registered channels.
-// Remember to call `close` when done with it, to allow the handler
-// to remove the channel from the list of registered clients.
-func (h *StreamHandler) OpenRx() *diRx {
-	c := make(chan *DI, 1)
-
-	// Inject last di processed to the new client.
-	h.lastDI.Lock()
-	// Inside the lock we'll get unique time values.
-	key := "stream:" + strconv.Itoa(int(time.Now().UnixNano()))
-	if di := h.lastDI.val; di != nil {
-		c <- di
-	}
-	h.lastDI.Unlock()
-
-	h.clients.Lock()
-	if h.clients.m == nil {
-		h.clients.m = make(map[string]chan *DI)
-	}
-	// Remove the client if was already there.
-	if val, ok := h.clients.m[key]; ok {
-		close(val)
-		delete(h.clients.m, key)
-	}
-	h.clients.m[key] = c
-	h.clients.Unlock()
-
-	return &diRx{
-		c: c,
-		close: func() {
-			defer close(c)
-
-			h.clients.Lock()
-			delete(h.clients.m, key)
-			h.clients.Unlock()
-		},
-	}
-}
-
-// copy/pasted from git.keepinmind.info/subgendsk/sgenc/trrec.go
-// Licensed under MIT, still not open source.
-// TODO: import the library as soon as it is available.
-func parseDuration(raw string) (time.Duration, error) {
-	parts := strings.Split(raw, ".")
-	if len(parts) != 2 {
-		return 0, fmt.Errorf("unable to split duration units from decimals")
-	}
-	units := strings.Split(parts[0], ":")
-
-	// Validation
-	if len(units) != 3 {
-		return 0, fmt.Errorf("duration units should be in the form of hh:mm:ss, found %s", parts[0])
-	}
-	for i, v := range units {
-		if len(v) != 2 {
-			return 0, fmt.Errorf("invalid number of digits at position %d: found %d, but only 2 is allowed", i, len(v))
-		}
-	}
-	if len(parts[1]) != 3 {
-		return 0, fmt.Errorf("invalid number of millisecond digits: found %d, but only 3 is allowed", len(parts[1]))
-	}
-
-	h, _ := strconv.Atoi(units[0])
-	m, _ := strconv.Atoi(units[1])
-	s, _ := strconv.Atoi(units[2])
-	ms, _ := strconv.Atoi(parts[1])
-
-	// Validation
-	if m > 59 {
-		return 0, fmt.Errorf("invalid minutes field: must be less than 59")
-	}
-	if s > 59 {
-		return 0, fmt.Errorf("invalid seconds field: must be less than 59")
-	}
-
-	d := time.Duration(0)
-	d += time.Duration(h) * time.Hour
-	d += time.Duration(m) * time.Minute
-	d += time.Duration(s) * time.Second
-	d += time.Duration(ms) * time.Millisecond
-
-	return d, nil
-}
-
-func makeCaption(w string, d time.Duration) string {
-	return fmt.Sprintf("\"%s\", heard after %v", w, d)
-}
-
-type mapset struct {
-	cs int // column start
-	ce int // column end
-	cw int // column word
-	cl int // column link
-}
-
-func (m *mapset) max() int {
-	max := 0
-	for _, v := range []int{m.cs, m.ce, m.cw, m.cl} {
-		if v > max {
-			max = v
-		}
-	}
-	return max
-}
-
-// 00:00:00.000,00:00:00.400,00:00:00.540,all,https://i.ytimg.com/vi/HAfFfqiYLp0/maxresdefault.jpg
-func decodeRecord(rec []string, m *mapset) (*DI, error) {
-	if len(rec) < m.max() {
-		return nil, fmt.Errorf("unexpected record length %d, need at least %d", len(rec), m.max())
-	}
-
-	uri := rec[m.cl]
-	u, err := url.ParseRequestURI(uri)
-	if err != nil {
-		return nil, fmt.Errorf("unable to recognise url at position %d: %w", m.cl, err)
-	}
-
-	d, err := parseDuration(rec[m.cs])
-	if err != nil {
-		return nil, fmt.Errorf("unable to parse record start time: %w", err)
-	}
-	word := rec[m.cw]
-
-	h := md5.New()
-	io.WriteString(h, uri)
-	fn := fmt.Sprintf("%s-%x%s", word, h.Sum(nil), filepath.Ext(u.Path))
-
-	return &DI{
-		FileName: fn,
-		Link:     uri,
-		Word:     word,
-		Caption:  makeCaption(word, d),
-	}, nil
-}
-
-func (h *StreamHandler) handleRecord(rec []string) (*DI, error) {
-	di, err := decodeRecord(rec, h.m)
-	if err != nil {
-		return nil, err
-	}
-
-	fn := filepath.Join(h.sd, di.FileName)
-
-	// If the file is already there, do not download again.
-	if _, err := os.Stat(fn); err == nil {
-		return di, nil
-	}
-
-	logf("downloading image for: %v", di.FileName)
-
-	f, err := os.Create(fn)
-	if err != nil {
-		return nil, fmt.Errorf("unable to prepare file for storing image: %w", err)
-	}
-	defer f.Close()
-
-	resp, err := http.Get(di.Link)
-	if err != nil {
-		return nil, fmt.Errorf("unable to download image: %w", err)
-	}
-	defer resp.Body.Close()
-	if _, err = io.Copy(f, resp.Body); err != nil {
-		return nil, fmt.Errorf("unable to store image: %w", err)
-	}
-
-	return di, nil
-}
-
-// Run keeps on reading from `h`'s internal reader, providing its
-// contents to the registered clients.
-func (h *StreamHandler) Run() {
-	logf("opening stream handler loop")
-	defer logf("closing stream handler loop")
-	r := csv.NewReader(h.r)
-
-	for {
-		// Read next record from input.
-		rec, err := r.Read()
-		if err != nil && errors.Is(err, io.EOF) {
-			logf("input was closed (%v), exiting", err)
-			os.Exit(0)
-		}
-		if err != nil {
-			exitf("unable to read from input: %v", err)
-		}
-		di, err := h.handleRecord(rec)
-		if err != nil {
-			errorf(err.Error())
-			continue
-		}
-
-		logf("---> %v", di.FileName)
-
-		// Send it to all clients.
-		h.clients.Lock()
-		for _, v := range h.clients.m {
-			v <- di
-		}
-		h.clients.Unlock()
-
-		// Save last di.
-		h.lastDI.Lock()
-		h.lastDI.val = di
-		h.lastDI.Unlock()
-	}
-}
-
-func wsError(ws *websocket.Conn, err error) {
-	logf("websocket error: %v", err)
-	ws.WriteMessage(websocket.TextMessage, []byte(err.Error()))
-}
-
-func (h *StreamHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	logf("connection from %v, %v", r.RemoteAddr, r.URL)
-	ws, err := h.up.Upgrade(w, r, nil)
-	if err != nil {
-		logf(err.Error())
-		return
-	}
-	defer func() {
-		logf("closing connection with %v", r.RemoteAddr)
-		ws.SetWriteDeadline(time.Now().Add(writeWait))
-		ws.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-		time.Sleep(closeGracePeriod)
-		ws.Close()
-	}()
-
-	rx := h.OpenRx()
-	defer rx.close()
-
-	for di := range rx.c {
-		if err := ws.WriteJSON(di); err != nil {
-			wsError(ws, err)
-			return
-		}
-	}
-}
-
-// NewStreamHandler returns a new http.Handler implementation that
-// supports websockets.
-func NewStreamHandler(in io.Reader, sd string, m *mapset) *StreamHandler {
-	upgrader := websocket.Upgrader{
-		ReadBufferSize:    4096,
-		WriteBufferSize:   4096,
-		EnableCompression: true,
-		CheckOrigin: func(r *http.Request) bool {
-			return true
-		},
-	}
-	h := &StreamHandler{
-		sd: sd,
-		r:  bufio.NewReader(in),
-		up: upgrader,
-		m:  m,
-	}
-	go h.Run()
-	return h
-}
-
-type fileHandler struct {
-	handler http.Handler
-}
-
-func FileServer(root http.FileSystem) http.Handler {
-	return &fileHandler{http.FileServer(root)}
-}
-
-func (f *fileHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	logf("file request from %v: %v", r.RemoteAddr, r.URL.Path)
-	f.handler.ServeHTTP(w, r)
-}
-
 func main() {
 	dsid := "dimages-" + strconv.Itoa(int(time.Now().Unix()))
 	i := flag.String("i", "-", "Input file path. Use - for stdin.")
-	wd := flag.String("wd", "."+arg0, "Image storage directory.")
-	sid := flag.String("s", dsid, "Session identifier. Images downloaded will be stored inside wd/sid.")
+	sd := flag.String("sd", dsid, "Storage directory path - where images will be stored.")
 	p := flag.Int("p", 7745, "Server listening port.")
 	cs := flag.Int("cs", 1, "Index of the column holding start information.")
 	ce := flag.Int("ce", 2, "Index of the column holding end information.")
@@ -385,24 +66,14 @@ func main() {
 	}
 	defer in.Close()
 
-	// Prepare storage directory.
-	dir := filepath.Join(*wd, *sid)
-	if err := os.MkdirAll(dir, os.ModePerm); err != nil {
-		exitf("unable to create storage directory %s: %v", dir, err)
-	}
+	// Register the file handler.
+	fh := api.NewFileHandler(*sd)
+	http.Handle("/di/images/", fh)
 
 	// Register stream handler.
-	h := NewStreamHandler(in, dir, &mapset{
-		cs: *cs,
-		ce: *ce,
-		cw: *cw,
-		cl: *cl,
-	})
-	http.Handle("/di/stream", h)
 
-	// Register the file handler.
-	fs := http.Dir(dir)
-	http.Handle("/di/images", FileServer(fs))
+	sh := api.NewStreamHandler(in, fh, api.NewMapSet(*cs, *ce, *cw, *cl))
+	http.Handle("/di/stream", sh)
 
 	// Configure server.
 	host := ":" + strconv.Itoa(*p)
@@ -425,13 +96,19 @@ func main() {
 	sigc := make(chan os.Signal, 1)
 	signal.Notify(sigc, os.Interrupt)
 
-	sig := <-sigc
+	select {
+	case sig := <-sigc:
+		logf("signal %v received, shutting down...", sig)
+	case err := <-sh.Done:
+		if err != nil {
+			errorf("closing websocket server: %v", err)
+		}
+	}
 
 	// If in the meanwhile stdin is closed, the server will serve the last
 	// content to the clients before exiting.
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 	defer cancel()
 
-	logf("signal %v received, shutting down...", sig)
 	srv.Shutdown(ctx)
 }
