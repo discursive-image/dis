f47e6d2322a9b7899afad6a65ceaf135c747a922
Daniel Morandini <danielmorandini@me.com>
2022-05-16 22:29:28 +0200

    Initialize flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "The Screaming Image Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.hello = pkgs.hello;
+	packages.dis = pkgs.buildGoModule {
+	  name = "dis";
+	  src = ./.;
+          vendorSha256 = "sha256-+bH4meSDxc8Fznf/ZuPQ7FNqtqTMLmAupmfPEOlCY1w=";
+	};
+
+        devShell = pkgs.mkShell { buildInputs = [ pkgs.hello pkgs.cowsay ]; };
+      });
+}
