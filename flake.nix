ac13e072bd91012ac69e2c27b1b69204775a1668
Daniel Morandini <danielmorandini@me.com>
2022-05-17 10:18:05 +0200

    Add nix formatting

--- flake.nix
+++ flake.nix
@@ -4,22 +4,28 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs-fmt.url = "github:nix-community/nixpkgs-fmt";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, nixpkgs-fmt }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        packages.hello = pkgs.hello;
-	packages.dis = pkgs.buildGoModule {
-	  name = "dis";
-	  src = ./.;
-          vendorSha256 = "sha256-+bH4meSDxc8Fznf/ZuPQ7FNqtqTMLmAupmfPEOlCY1w=";
-	};
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        packages = {
+          dis = pkgs.buildGoModule {
+            name = "dis";
+            src = ./.;
+            vendorSha256 = "sha256-+bH4meSDxc8Fznf/ZuPQ7FNqtqTMLmAupmfPEOlCY1w=";
+          };
+        };
 
-        devShell = pkgs.mkShell { buildInputs = with pkgs; [
-  	  go
-	  gopls
-        ];};
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+            gopls
+          ];
+        };
       });
 }
